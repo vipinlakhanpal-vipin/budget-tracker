6218f65329a2ce90c34fe5c4a212d535a391d6a7
vipinlakhanpal-vipin <vipinlakhanpal@gmail.com>
2026-07-30 12:40:54 +0400

    Add files via upload

--- src/version.js
+++ src/version.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = '1.83';
+export const APP_VERSION = '1.84';
 export function formatVersionBadge() {
 const dateLabel = new Date().toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' });
 return `${dateLabel} - v${APP_VERSION}`;
